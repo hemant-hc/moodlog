d99470221422897fca3036eb0bb1b72113ecc5ff
hemant-hc <iamhemantchaudhary1@gmail.com>
2024-03-10 00:25:31 +0530

    history chart functionality

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -1,10 +1,14 @@
 const EntryCard = ({ entry }) => {
   const date = new Date(entry.createdAt).toDateString();
+  const contentWords = entry.content.split(" ");
+  const contentPreview =
+    contentWords.length > 15
+      ? `${contentWords.slice(0, 15).join(" ")} ...`
+      : entry.content;
   return (
     <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
       <div className="px-4 py-5">{date}</div>
-      <div className="px-4 py-5">summary</div>
-      <div className="px-4 py-4">Mood</div>
+      <div className="px-4 py-5">Preview: {contentPreview}</div>
     </div>
   );
 };
